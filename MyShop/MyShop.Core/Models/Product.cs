ca5bc7e10656507408119986a861816de491d052
NikoloDimitris <dimitrisnikolo@hotmail.com>
2018-12-06 11:27:43 +0200

    Product Model and In Memory Product Repository Built

---
+++ MyShop/MyShop.Core/Models/Product.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.Core.Models
+{
+    public class Product
+    {
+        public string Id { get; set; }
+
+        [StringLength(20)]
+        [DisplayName("Product Name")]
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        [Range (0,1000)]
+        public decimal Price { get; set; }
+        public string Category { get; set; }
+        public string Image { get; set; }           //will contain a url
+
+        //create a constructor so that very time we create an instance of product we automatically generate an id
+        public Product() {
+            this.Id = Guid.NewGuid().ToString();        //initially a generate Id & turns that ToString the Id
+        }
+
+    }
+}
---
+++ MyShop/MyShop.DataAccess.InMemory/ProductRepository.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Caching;
+using MyShop.Core.Models;
+
+namespace MyShop.DataAccess.InMemory
+{
+    public class ProductRepository
+    {
+        ObjectCache cache = MemoryCache.Default;
+        //create an internal list of products
+        List<Product> products; // = new List<Product>(); to afairei dioti to prosthetei parakatw, line22
+
+        //create a constructor
+        public ProductRepository() {
+
+            products = cache["products"] as List<Product>;
+            if(products==null) {
+                products = new List<Product>();
+                //whenever we launch this, we are going to try to look in the cache to see if there is a cache call products
+            }     
+        }
+
+        public void Commit()
+        {
+            cache["products"] = products;
+        }
+
+        //create an individual end point to insert, delete, find, edit, return with list fuctionallity
+        public void Insert (Product p) {
+            products.Add(p);
+        }
+
+        public void Update (Product product) {              //take in a product
+            Product productToUpdate = products.Find(p => p.Id == product.Id);   //find the product we want to update
+
+            if (productToUpdate != null) {      //make sure we have a product
+                productToUpdate = product;      //if true we copy the information from the product we send in
+            }
+            else {
+                throw new Exception("Product no found");
+            }
+        }
+
+        public Product Find (string Id) {
+            Product product = products.Find(p => p.Id == Id);   
+
+            if (product != null)
+            {      
+                return product;      
+            }
+            else
+            {
+                throw new Exception("Product no found");
+            }
+        }
+
+
+        //return a list of products queried!!
+        public IQueryable<Product> Collection() {
+            return products.AsQueryable();
+        }
+
+        public void Delete (string Id) {
+            Product productToDelete = products.Find(p => p.Id == Id);   
+
+            if (productToDelete != null)
+            {
+                products.Remove(productToDelete);      
+            }
+            else
+            {
+                throw new Exception("Product no found");
+            }
+        }
+
+
+    }
+}
