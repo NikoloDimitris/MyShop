bfce521f205b3236d0a85912dbdcb5a7066d7900
NikoloDimitris <dimitrisnikolo@hotmail.com>
2018-12-09 16:59:19 +0200

    Addded Entity Framework

---
+++ MyShop/MyShop.DataAccess.SQL/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using MyShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.DataAccess.SQL
+{
+    public class DataContext : DbContext
+    {
+        //the first thing is to create a constructor so that we capture & passing that connection string that the base class is expecting
+        public DataContext()
+            :base("DefaultConnection") {
+
+        }
+        
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+    }
+}
